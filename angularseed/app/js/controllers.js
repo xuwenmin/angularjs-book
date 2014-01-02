7024c70388138e600596a0d29062d4c352a6a849
xuwenmin <151481184@qq.com>
2014-01-02 19:07:13 +0800

    Signed-off-by: xuwenmin <151481184@qq.com>

--- angularseed/app/js/controllers.js
+++ angularseed/app/js/controllers.js
@@ -3,8 +3,8 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-controller('MyCtrl1', ["$scope","utilconfig",'service1','service2','service3',
-  function($scope,utilconfig,service1,service2,service3) {
+controller('MyCtrl1', ["$scope","utilconfig",'service1','service2','service3','$window',
+  function($scope,utilconfig,service1,service2,service3,$window) {
       $scope.constant=utilconfig.xuwm; //从constant中定义的常量服务中去取属性
       $scope.service1_v=service1.v; //service1 默认是返回[执行了function 的返回值]      用factory 定义的service,
       $scope.service2_v=service2.v; //service2 默认是angularjs执行new service2 返回一个以后一直用的实例   用service定义的service
@@ -14,7 +14,8 @@ controller('MyCtrl1', ["$scope","utilconfig",'service1','service2','service3',
 
       $scope.isshow = true;
       $scope.test=function(){
-        console.log($scope);
+         // $scope.$emit("demo.add");
+         // $scope.$broadcast("demo.add");
       };
       $scope.expanders = [
         {
@@ -30,9 +31,23 @@ controller('MyCtrl1', ["$scope","utilconfig",'service1','service2','service3',
           text: 'test'
         }
       ];
+
+      $scope.$on("parent.add",function(){
+          $scope.constant='demo emit';
+      });
+
+      // $scope.$broadcast是为了触发子节点设置的事件
+
   }
 ])
-  .controller('MyCtrl2', ['$scope',
+.controller('Xuwm',['$scope','$window',function($scope,$window){
+
+    // $scope.$emit是为了触发父节点设置的事件以及本身内设置的事件
+    $scope.emitevent=function(){
+        $scope.$emit('parent.add');
+    }
+}])
+.controller('MyCtrl2', ['$scope',
     function($scope) {
         //监控desc 属性，下面是不管输入什么，都只显示aaa
         var watch_desc=$scope.$watch("desc",function(newval,oldval,scope){
@@ -42,4 +57,4 @@ controller('MyCtrl1', ["$scope","utilconfig",'service1','service2','service3',
           watch_desc();
         };
     }
-  ]);
+]);
